e2a384d0c64446fa2ebffe8dc24091878c106676
Ayoade Ibrahim <ayoadeibrahim84@gmail.com>
2020-03-21 18:23:41 +0100

    JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+let fullName = "Ayoade Ibrahim";
+
+let myCourses = ["HTML", "CSS", "JavaScript", "UI/UX", "Coding"];
+
+console.log("My name is " + name +" and I offer these Start.ng courses: " + myCourses);
+
+
+for(x = 1; x <= 200; x += 2) {
+    
+    console.log(x);
+}
